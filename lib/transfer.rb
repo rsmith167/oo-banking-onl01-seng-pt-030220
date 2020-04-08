cbec84e9f8561a464a6e3fdd547b1833fe6c5464
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 18:28:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,6 +15,13 @@ class Transfer
       FALSE
     end
   end
-  def execute_transacction
+  def execute_transaction
+    if self.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      status = "executed"
+  end
+  def reverse_transaction
     
+  end
 end
