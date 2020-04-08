02c2ae10564c01e5928f7517152275b85c074f6e
Rwanda S <rwanda.ace@gmail.com>
2020-04-08 18:41:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,10 +21,14 @@ class Transfer
       @receiver.deposit( @amount)
       @status = "complete"
     else
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
-  def reverse_transaction
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit(-@amount)
+      @receiver.deposit( @amount)
     
   end
 end
